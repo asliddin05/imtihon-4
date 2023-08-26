a39648a1bea0093ac8e3708691a2f9871dd90602
asliddin05 <asliddinalimov05@gmail.com>
2023-08-26 11:16:38 +0500

    sdfsdf

---
+++ main.js
@@ -0,0 +1,29 @@
+// misol 1
+
+// let arr = ["I","study","JavaScript","right","now"];
+// function delette(arr, del){
+//     return arr.filter(right => right !== del);
+// }
+// let arr2 = delette(arr,"right");
+// console.log(arr2);
+
+// misol 2
+
+// let str = "Salom dunyo";
+
+// console.log(str.split("").reverse().join(""));
+
+// misol 3
+
+// const str = 'jdsfd23j234hbsdf324';
+// let arr = [];
+// for(let i = 0; i < str.length; i++){
+//     if(str[i] == 'number'){
+//         arr.push(str[i]);
+//     }
+// }
+// console.log(toString(arr));
+// const replaced = str.replace(/[0-9]/g, '');
+// console.log(replaced);
+
+// arrayga sola olmadim
